45fa8a7c1260045cf76ba5c8fff016036593cbf1
Covenant Monday <covenantmonday863@gmail.com>
2025-11-05 21:29:25 +0100

    updated main

---
+++ router/user.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends
+from db import db_user
+from db.database import get_db
+from schemas import UserBase, UserDisplay
+from sqlalchemy.orm import Session
+
+router = APIRouter(
+    prefix='/user',
+    tags=['user']
+)
+
+# create
+@router.post("/create", response_model=UserDisplay)
+def create_user(request: UserBase, db: Session = Depends(get_db)):
+    return db_user.create_user(db, request)
+
+
+# read
+
+
+# update
+
+
+# delete
