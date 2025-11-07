ef965ba37cde9a227c8a1d2599549d633c2efd72
Covenant Monday <covenantmonday863@gmail.com>
2025-11-07 22:23:10 +0100

    added more endpoints

--- router/user.py
+++ router/user.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, Depends
 from db import db_user
 from db.database import get_db
@@ -16,7 +17,15 @@ def create_user(request: UserBase, db: Session = Depends(get_db)):
 
 
 # read
+@router.get("/all", response_model=List[UserDisplay])
+def get_all_users(db: Session = Depends(get_db)):
+    return db_user.get_all_users(db)
 
+
+# read one user
+@router.get("/{id}", response_model=UserDisplay)
+def get_user(id: int, db: Session = Depends(get_db)):
+    return db_user.get_user(db, id)
 
 # update
 
