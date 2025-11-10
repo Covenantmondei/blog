153f388693c185f093133f7d0e2412bd1c2f1dff
Covenant Monday <covenantmonday863@gmail.com>
2025-11-10 17:23:50 +0100

    created routes

---
+++ router/article.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends
+from db import db_article
+from db.database import get_db
+from schemas import ArticleBase, ArticleDisplay
+from sqlalchemy.orm import Session
+
+router = APIRouter(
+    prefix='/article',
+    tags=['article']
+)
+
+# create article
+@router.post("/create", response_model=ArticleDisplay)
+def create_article(request: ArticleBase, db: Session = Depends(get_db)):
+    return db_article.create_article(db, request)
+
+
+# get specific article
+@router.get("/{id}", response_model=ArticleDisplay)
+def get_article(id: int, db: Session = Depends(get_db)):
+    return db_article.get_article(db, id)
