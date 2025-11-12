5655959e7c2174a1fc542f5f36fa1079a5475e29
Covenant Monday <covenantmonday863@gmail.com>
2025-11-12 12:38:11 +0100

    updated main

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
-from router import article, blog_get, blog_post, user
+from router import article, blog_get, blog_post, user, product
 from db import models
 from db.database import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 app.include_router(user.router)
 app.include_router(article.router)
+app.include_router(product.router)
+
 
 @app.get("/")
 def index():
@@ -15,3 +18,13 @@ def index():
 
 
 models.Base.metadata.create_all(engine)
+
+origins = ['localhost:3000']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ['*'],
+    allow_headers = ['*']
+)
