111c27c11d740255a35873f25c514a3e160d6153
Covenant Monday <covenantmonday863@gmail.com>
2025-11-13 13:57:48 +0100

    updated main

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
+from auth import authentication
 from router import article, blog_get, blog_post, user, product
 from db import models
 from db.database import engine
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.include_router(authentication.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 app.include_router(user.router)
